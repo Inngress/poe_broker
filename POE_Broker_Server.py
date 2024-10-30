d51f9b586644ac3e3cad4ade4f1b7bb332b161e4
Inngress <74958967+Inngress@users.noreply.github.com>
2024-10-30 21:34:23 +0100

    First working iteration
    Thanks Benjamin

--- POE_Broker_Server.py
+++ POE_Broker_Server.py
@@ -8,21 +8,34 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        gem_url = 'https://poe.ninja/api/data/itemoverview?league=Settlers&type=SkillGem'
-        gem_res = rq.get(gem_url)
-        gem_res_dict = gem_res.json()
-        
-        
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        # self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        # self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("This is an example web server.", "utf-8"))
-        # self.wfile.write(bytes(f"<p>{gem_res}</p>", "utf-8"))
-        # self.wfile.write(bytes("</body></html>", "utf-8"))
+        if self.path == '/':
+            self.sendresponse(200)
+            self.sendheader("Content-type", "text/html")
+            self.endheaders()
+            with open('POE_Broker_Front.html', 'r') as file:
+                self.wfile.write(bytes(file.read(), "utf-8"))
 
+        elif self.path == '/api':
+            gem_url = 'https://poe.ninja/api/data/itemoverview?league=Settlers&type=SkillGem'
+            gem_res = rq.get(gem_url)
+            gem_res_dict = gem_res.json()
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")  # CORS header
+            self.end_headers()
+
+            self.wfile.write(bytes(json.dumps(gem_res_dict), "utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(
+                "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>", "utf-8"))
+        
+        
+        
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
